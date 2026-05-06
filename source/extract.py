6efb914a251f171c08359b9c24eb25bab264580e
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-05-05 21:44:06 -0300

    Updating extract.py

--- source/extract.py
+++ source/extract.py
@@ -1,1 +1,6 @@
+import pandas as pd
+import os
 
+endereco_csv = os.path.join (os.path.dirname (__file__), "..", "data-frame", "original", "car_price_dataset.csv")
+df = pd.read_csv (endereco_csv)
+print ("Arquivo csv carregado")
