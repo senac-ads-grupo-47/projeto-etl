9835280b7c8ecd2e5eff2b66b535dac9e36c60d6
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-05-11 11:32:56 -0300

    Adicionando tratamento de erros ao extract.py

--- source/extract.py
+++ source/extract.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import os
 
-endereco_csv = os.path.join (os.path.dirname (__file__), "..", "data-frame", "original", "car_price_dataset.csv")
-df = pd.read_csv (endereco_csv)
-print ("Arquivo csv carregado")
+def extract():
+	endereco_csv = os.path.join (os.path.dirname (__file__), "..", "data-frame", "original", "car_price_dataset.csv")
+	try:
+		df = pd.read_csv(endereco_csv)
+		print ("Arquivo csv carregado")
+		return df
+	except FileNotFoundError:
+		raise Exception (f"Arquivo csv não encontrado: {endereco_csv}")
+
+if __name__ == "__main__":
+	df = extract()
