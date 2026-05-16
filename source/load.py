d9ff69d96d286ab60e6a8339ce1e8c13b4eb8897
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-05-16 11:50:30 -0300

    Criando load.py. Adicionando regras no .gitignore

--- source/load.py
+++ source/load.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import os
+from transform import transform
 
+def load (df):
+	endereco_csv = os.path.join (os.path.dirname (__file__), "..", "data-frame", "modifified", "car_price_dataset_mod.csv")
+	try:
+		df.to_csv (endereco_csv, index = False, header = True)
+		print ("Arquivo csv criado")
+		return df
+	except FileNotFoundError:
+		raise Exception (f"Diretório não existe: {endereco_csv}")
+
+if __name__ == '__main__':
+	df = transform()
+	df_criado = load (df_modificado)
